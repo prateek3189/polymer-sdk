843e87d80667c841b7ca7b9b5e9839dd9710642f
prateek3189 <prateek.magarde@gmail.com>
2019-12-22 21:47:56 +0530

    Polymer-sdk updates 3.0

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,21 @@
-import { MyApp } from './app/index';
 
-customElements.define('my-app', MyApp);
+
+import {
+  LitElement, html, customElement, property
+} from 'lit-element';
+
+import * as view from "./index.template.html";
+
+@customElement('my-app')
+export class MyApp extends LitElement {
+
+  @property()
+  foo = 'foo';
+
+  render(){
+    return html`<p>${this.foo}</p>`;
+  }
+}
+
+// Another way of doing this
+// customElements.define('my-app', MyApp);
